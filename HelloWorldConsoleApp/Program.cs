f9dc8b886412ec98e9f20393426ed47f14093eb7
thecodingbuddhagithub <74766682+thecodingbuddhagithub@users.noreply.github.com>
2020-11-20 15:32:19 +0530

    Add files via upload

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("\nWho are you there?");
+            Console.WriteLine("\nwaiting...for your good name.");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
